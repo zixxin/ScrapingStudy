c162f1f9a9f322dcdca6c5aef04d0f67a9e12e35
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2021-11-07 14:37:29 +0900

    week 11 finished

--- 5_user_agent.py
+++ 5_user_agent.py
@@ -1,7 +1,9 @@
 import requests
 
-res = requests.get("https://hisnet.handong.edu")
-# res.raise_for_status()
+url = "http://nadocoding/tistory.com"
+headers = {"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.1 Safari/605.1.15"}
+res = requests.get(url, headers=headers)
+res.raise_for_status()
 
 with open("nadocoding.html", "w", encoding="utf8") as f: 
     f.write(res.text)
