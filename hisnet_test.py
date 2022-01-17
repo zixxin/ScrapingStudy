e80cf68a49f5ad9e27cc08286903c21fb2e5643f
윤지인 <74345861+22000487zixxin@users.noreply.github.com>
2022-01-17 11:40:54 +0900

    20220117 study전 commit

--- hisnet_test.py
+++ hisnet_test.py
@@ -37,14 +37,14 @@ time.sleep(1)
 
 # 히즈넷 로그인 성공 후 메인화면까지 들어온 상태
 
-# # 일반 공지 메뉴 클릭
-# browser.find_element_by_xpath('//*[@id="td_box22_img"]').click()
+# 일반 공지 메뉴 클릭
+browser.find_element_by_xpath('//*[@id="td_box22_img"]').click()
 # # 학부 공지 메뉴 클릭
 # browser.find_element_by_xpath('//*[@id="td_box23_img"]').click()
 # # 장학 공지 메뉴 클릭
 # browser.find_element_by_xpath('//*[@id="td_box24_img"]').click()
-# 취업 공지 메뉴 클릭
-browser.find_element_by_xpath('//*[@id="td_box25_img"]').click()
+# # 취업 공지 메뉴 클릭
+# browser.find_element_by_xpath('//*[@id="td_box25_img"]').click()
 
 # 더보기 클릭
 browser.find_element_by_xpath('/html/body/table[2]/tbody/tr/td[1]/table[1]/tbody/tr/td[2]/div/a/img').click()
@@ -55,7 +55,7 @@ time.sleep(1)
 
 # 취업 공지 게시판은 for i in range(16,31): 으로 실행 / 나머지 공지 게시판은 for i in range(17,32): 으로 실행
 # 1페이지 내에 있는 모든 공지 목록 클릭하여 세부정보 받아오기
-for i in range(16,31):
+for i in range(17,32):
     # 공지 제목 클릭
     browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[{0}]/td[2]/a'.format(i)).click()
     # # 공지 번호 클릭
@@ -86,78 +86,78 @@ for i in range(16,31):
     browser.find_element_by_xpath('//*[@id="divReadButton"]/a/img').click()
     time.sleep(1)
 
-# # 다음 페이지로 넘어가기
-# # 일반공지 5페이지까지 업로드!
-# # 취업 공지 게시판은 4페이지까지 업로드!
-# # 학부공지(전전) / 장학공지 게시판은 1페이지만 업로드!
-for i in range(1,4):
-    # 1페이지에 있을 때, 02는 a[1], (2페이지 이상) n페이지에 있을 때, 다음은 a[n+1]
-    if i == 1:
-        browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[4]/td/table/tbody/tr/td/div/a[1]/font').click()
-        time.sleep(1)
+# # # 다음 페이지로 넘어가기
+# # # 일반공지 5페이지까지 업로드!
+# # # 취업 공지 게시판은 4페이지까지 업로드!
+# # # 학부공지(전전) / 장학공지 게시판은 1페이지만 업로드!
+# for i in range(1,4):
+#     # 1페이지에 있을 때, 02는 a[1], (2페이지 이상) n페이지에 있을 때, 다음은 a[n+1]
+#     if i == 1:
+#         browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[4]/td/table/tbody/tr/td/div/a[1]/font').click()
+#         time.sleep(1)
 
-        for i in range(16,31):
-        # 공지 제목 클릭
-            browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[{0}]/td[2]/a'.format(i)).click()
-            # # 공지 번호 클릭
-            # browser.find_element('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[17]/td[1]/div/a')
-            time.sleep(1)
+#         for i in range(17,31):
+#         # 공지 제목 클릭
+#             browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[{0}]/td[2]/a'.format(i)).click()
+#             # # 공지 번호 클릭
+#             # browser.find_element('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[17]/td[1]/div/a')
+#             time.sleep(1)
 
-            print("\n")
-            noti_title = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/span[2]")
-            print("--- "+ noti_title.text +" ---\n")
-            noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[3]/td/table/tbody/tr/td")
+#             print("\n")
+#             noti_title = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/span[2]")
+#             print("--- "+ noti_title.text +" ---\n")
+#             noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[3]/td/table/tbody/tr/td")
 
-            for i in range(1,6):
-                if noti_info.text.encode('UTF-8', 'ignore') == "첨부 #1":
-                    try:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+3))
-                    except NoSuchElementException:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
-                else:
-                    try:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+2))
-                    except NoSuchElementException:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
-                    break
+#             for i in range(1,6):
+#                 if noti_info.text.encode('UTF-8', 'ignore') == "첨부 #1":
+#                     try:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+3))
+#                     except NoSuchElementException:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
+#                 else:
+#                     try:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+2))
+#                     except NoSuchElementException:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
+#                     break
 
-            print(noti_info.text)
-            print("\n")
+#             print(noti_info.text)
+#             print("\n")
 
-            browser.find_element_by_xpath('//*[@id="divReadButton"]/a/img').click()
-            time.sleep(1)
+#             browser.find_element_by_xpath('//*[@id="divReadButton"]/a/img').click()
+#             time.sleep(1)
     
-    else:
-        browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[4]/td/table/tbody/tr/td/div/a[{0}]/font'.format(i+1)).click()
-        time.sleep(1)
+#     else:
+#         browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[4]/td/table/tbody/tr/td/div/a[{0}]/font'.format(i+1)).click()
+#         time.sleep(1)
 
-        for i in range(16,31):
-            # 공지 제목 클릭
-            browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[{0}]/td[2]/a'.format(i)).click()
-            # # 공지 번호 클릭
-            # browser.find_element('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[17]/td[1]/div/a')
-            time.sleep(1)
+#         for i in range(16,31):
+#             # 공지 제목 클릭
+#             browser.find_element_by_xpath('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[{0}]/td[2]/a'.format(i)).click()
+#             # # 공지 번호 클릭
+#             # browser.find_element('/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table/tbody/tr[1]/td/table/tbody/tr[17]/td[1]/div/a')
+#             time.sleep(1)
 
-            print("\n")
-            noti_title = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/span[2]")
-            print("--- "+ noti_title.text +" ---\n")
-            noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[3]/td/table/tbody/tr/td")
+#             print("\n")
+#             noti_title = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[1]/td/table/tbody/tr/td[1]/div/span[2]")
+#             print("--- "+ noti_title.text +" ---\n")
+#             noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[3]/td/table/tbody/tr/td")
 
-            for i in range(1,6):
-                if noti_info.text.encode('UTF-8', 'ignore') == "첨부 #1":
-                    try:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+3))
-                    except NoSuchElementException:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
-                else:
-                    try:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+2))
-                    except NoSuchElementException:
-                        noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
-                    break
+#             for i in range(1,6):
+#                 if noti_info.text.encode('UTF-8', 'ignore') == "첨부 #1":
+#                     try:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+3))
+#                     except NoSuchElementException:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
+#                 else:
+#                     try:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[{0}]/td/table/tbody/tr/td".format(i+2))
+#                     except NoSuchElementException:
+#                         noti_info = browser.find_element_by_xpath("/html/body/table[1]/tbody/tr[2]/td/table/tbody/tr/td[3]/table/tbody/tr[3]/td/table[1]/tbody/tr[1]/td/table/tbody/tr[5]/td/table/tbody/tr/td")
+#                     break
 
-            print(noti_info.text)
-            print("\n")
+#             print(noti_info.text)
+#             print("\n")
 
-            browser.find_element_by_xpath('//*[@id="divReadButton"]/a/img').click()
-            time.sleep(1)
+#             browser.find_element_by_xpath('//*[@id="divReadButton"]/a/img').click()
+#             time.sleep(1)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+# 취업 공지 메뉴 클릭
+# browser.find_element_by_xpath('//*[@id="td_box25_img"]').click()
